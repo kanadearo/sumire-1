e109b191dc0360415a4639c486a9923c3723d7d1
kanade <futae.0328@gmail.com>
2018-06-14 01:56:26 +0900

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"]
+      region: 'us-east-1'
     }
 
     config.storage = :fog
     config.asset_host = 'https://s3.amazonaws.com/su-mire'
-    config.fog_directory = ENV["AWS_BUCKET_NAME"],
+    config.fog_directory = 'su-mire'
     config.cache_storage = :fog
   end
 end
