4605f664e239bad68b1dfc65ce4489d594dff101
kanade <futae.0328@gmail.com>
2018-06-22 14:08:08 +0900

    for heroku

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,13 +5,13 @@ class RelationshipsController < ApplicationController
     user = User.find_by(id: params[:follow_id])
     current_user.follow(user)
     flash[:success] = 'ユーザーをフォローしました。'
-    redirect_to user
+    redirect_to facebook_users_user_searchs
   end
 
   def destroy
     user = User.find_by(id: params[:id])
     current_user.unfollow(user)
     flash[:success] = 'ユーザーのフォローを解除しました。'
-    redirect_to user
+    redirect_to facebook_users_user_searchs
   end
 end
